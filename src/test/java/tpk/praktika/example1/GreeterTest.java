785e74ea7b537456c97dd5c081b2c8f25c4f4619
faa <fomich-artem@ya.ru>
2018-06-10 10:16:36 +0400

    some comments

--- src/test/java/tpk/praktika/example1/GreeterTest.java
+++ src/test/java/tpk/praktika/example1/GreeterTest.java
@@ -1,5 +1,6 @@
 package tpk.praktika.example1;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,18 +11,33 @@ public class GreeterTest {
 	
 	@Before
 	public void before() {
+		// здесь мы подготавливаем объекты, которые будут использоваться в тестовом методе
+		// это может быть, например, подключение к БД
+		// для примера получим объект greeter
 		greeter = new Greeter();
 	}
 
-	@Test
+	@After
+	public void after() {
+		// здесь мы выполняем действия, которые должны быть выполнены после выполнения тестового метода
+		// (если это требуется, например, закрытие подключения к БД)
+		// для примера просто сбросим greeter в null
+		greeter = null;
+	}
+
+	@Test // объявляем тестовый метод
 	public void testHelloWorld() {
+		// вызываем интересующий нас метод, передаем необходимые параметры
 		String helloResult = greeter.hello("World");
+		// проверяем результат вызова
 		Assert.assertEquals("Hello, World!", helloResult);
 	}
 
 	@Test
 	public void testHelloJava() {
+		// вызываем интересующий нас метод, передаем необходимые параметры
 		String helloResult = greeter.hello("Java");
+		// проверяем результат вызова
 		Assert.assertEquals("Hello, Java!", helloResult);
 	}
 
